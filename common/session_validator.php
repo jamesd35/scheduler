cffc4bb9647969ac6ef82870356f53c3a813db25
jamesd35 <jamesd35@users.noreply.github.com>
2018-07-28 08:46:45 -0500

    Update session_validator.php

--- common/session_validator.php
+++ common/session_validator.php
@@ -7,27 +7,27 @@ error_log("test", 0);
 	if so it adds more time to the cookie and returns, else it loops them back to the login page and exits the script so nothing can be changed at all. It is called at the top of every
 	page.
 */
-	$dbhost = getenv("MYSQL_SERVICE_HOST");
-	$dbport = getenv("MYSQL_SERVICE_PORT");
-	$dbuser = getenv("databaseuser");
-	$dbpwd = getenv("databasepassword");
-	$dbname = getenv("databasename");
-	$con = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 
 	$validation_url = "http://$_SERVER[HTTP_HOST]/common/onyen_validator.php";
 
 	function getDatabaseConnection() {
 		global $con;
+		$dbhost = getenv("MYSQL_SERVICE_HOST");
+		$dbport = getenv("MYSQL_SERVICE_PORT");
+		$dbuser = getenv("databaseuser");
+		$dbpwd = getenv("databasepassword");
+		$dbname = getenv("databasename");
+		$con = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 		return $con;
 	}
 
 	session_start();
-	
+/*	
 	if(!isset($_COOKIE['TutorSchedulerAuth']) || $_COOKIE['TutorSchedulerAuth'] != md5($_SESSION['pid'] . $_SESSION['remote_address'] . $_SERVER['REMOTE_ADDR'] . $_SESSION['authsalt'])) {
 		//meaning the authorization cookie either isn't there, the session cookie isn't there, or the cookie doesn't match what we expect
 		echo "<script type = 'text/javascript'>location.href='$validation_url'</script>";
 		exit();
-	}
+	}*/
 	//otherwise, we'll update the cookie expiration, regenerate the session id, and whatever page we got here from will continue as per usual.
 	session_regenerate_id(true);
 	setcookie('TutorSchedulerAuth', $_COOKIE['TutorSchedulerAuth'], time()+3600, '/', $_SERVER['SERVER_NAME'], 1);	//cookie now doesn't expire for another hour
